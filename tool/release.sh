e79ec260faf1d9532d8e12d7931e2e993982c7f1
Jordon de Hoog <jordon.dehoog@gmail.com>
2019-07-17 21:00:57 -0400

    ci(release): add a script for releasing the library

---
+++ tool/release.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+TAG="Version"
+CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+PARENT="$(cd $CWD/.. >/dev/null 2>&1 && pwd)"
+
+set -e
+
+if [ -z "$1" ]; then
+    echo "$TAG: No version was passed in!"
+    exit 1
+fi
+
+cd $PARENT
+
+echo "Updating pubspec with new version $1"
+node ./tools/update_version --ver $1
+retVal=$?
+if [ ! $retVal -eq 0 ]; then
+  echo "Failed to update the version!"
+  exit 1
+fi
+
+echo "Publishing version $1 to pub.dev"
+flutter pub publish
+retVal=$?
+if [ ! $retVal -eq 0 ]; then
+  echo "Failed to publish to pub.dev!"
+  exit 1
+fi
+
+echo "Finished"
