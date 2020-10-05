0d4a07540d73e6057f990e02b7f528bb24d54753
Silver Aabjõe <ristiisa@gmail.com>
2020-10-05 11:23:49 +0300

    fixed code generation to combat tree shaking error

--- example/lib/util.dart
+++ example/lib/util.dart
@@ -1,5 +1,5 @@
 class WeatherIconsUtil {
-  static const int dateTimestamp = 1563582063844;
+  static const int dateTimestamp = 1601885646647;
 
   static const Map<String, int> iconMap = {
     'wi-day-sunny': 0xf00d,
